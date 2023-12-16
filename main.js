b7f2eed52af08ef75fe7dc92d710176c56f2adca
Daniel Carrizo <hdcarrizo30@gmail.com>
2023-12-16 11:26:30 -0300

    metodos creados

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises'); // Import the promises version of the fs module
+const fs = require('fs/promises');
 
 class ProductManager {
   constructor() {
@@ -7,23 +7,43 @@ class ProductManager {
 
   static id = 0;
 
-  async addProduct(title, description, price, image, code, stock) {
+  // Method to add a product to the array with auto-incremented id
+  async addProduct(productData) {
+    // Increment the static id for each new product
     ProductManager.id++;
-    this.products.push({ title, description, price, image, code, stock, id: ProductManager.id });
+    
+    // Create a new product object with the provided data and the auto-incremented id
+    const newProduct = {
+      ...productData,
+      id: ProductManager.id
+    };
 
-    await this.writeToFile(); // Save products to file after adding a new product
+    // Add the new product to the products array
+    this.products.push(newProduct);
+    
+    // Save the updated products array to the file after adding a new product
+    await this.writeToFile();
+
+    // Return the newly added product with the assigned id
+    return newProduct;
   }
 
+  // Method to get the list of products
   async getProducts() {
-    await this.readFromFile(); // Read products from file before returning
+    // Read products from the file before returning the list
+    await this.readFromFile();
     return this.products;
   }
 
+  // Method to check if a product with a specific id exists
   async getProductById(id) {
-    await this.readFromFile(); // Read products from file before searching
+    // Read products from the file before searching
+    await this.readFromFile();
 
+    // Find the product with the specified id
     const foundProduct = this.products.find((product) => product.id === id);
 
+    // Log whether the product was found or not
     if (!foundProduct) {
       console.log('Not found');
     } else {
@@ -31,6 +51,7 @@ class ProductManager {
     }
   }
 
+  // Method to write the products array to the file
   async writeToFile() {
     try {
       await fs.writeFile('products.json', JSON.stringify(this.products, null, 2));
@@ -39,12 +60,13 @@ class ProductManager {
     }
   }
 
+  // Method to read products from the file into the products array
   async readFromFile() {
     try {
       const data = await fs.readFile('products.json', 'utf-8');
       this.products = JSON.parse(data);
     } catch (error) {
-      // If the file doesn't exist or an error occurs, it will be handled here
+      // If the file doesn't exist or an error occurs, handle it here
       if (error.code === 'ENOENT') {
         console.log('File not found. Creating a new one.');
       } else {
@@ -59,9 +81,17 @@ const productManager = new ProductManager();
 
 // Example usage
 (async () => {
-  await productManager.addProduct('titulo1', 'descripcion1', 1000, 'imagen1', 'abc123', 5);
-  await productManager.addProduct('titulo2', 'descripcion2', 1000, 'imagen2', 'abc123', 5);
+  // Add a new product
+  await productManager.addProduct({
+    title: 'Test Product',
+    description: 'This is a test product',
+    price: 200,
+    thumbnail: 'Test Image',
+    code: 'test123',
+    stock: 10
+  });
 
+  // Get the list of products and log it
   const products = await productManager.getProducts();
   console.log(products);
 
